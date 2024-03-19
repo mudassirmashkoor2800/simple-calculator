2e07f1f3630844b09cbad8f537938a21f7b4a57c
mudassirmashkoor2800 <mudassirmashkoor@gmail.com>
2024-03-20 03:02:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select One of The Operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(`Your answer is ${answer.firstNumber + answer.secondNumber}`);
+}
+else if (answer.operator === "Substraction") {
+    console.log(`Your answer is ${answer.firstNumber - answer.secondNumber}`);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(`Your answer is ${answer.firstNumber * answer.secondNumber}`);
+}
+else if (answer.operator === "Division") {
+    console.log(`Your answer is ${answer.firstNumber / answer.secondNumber}`);
+}
+else {
+    console.log("You choose wrong Operator");
+}
